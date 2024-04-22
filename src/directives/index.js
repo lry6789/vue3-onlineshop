6b191efaec0030c485599bff124afe51f3edb9f1
lry6789 <cleansexd1@gmail.com>
2024-04-22 13:40:36 +0800

    插件注册监听位置

---
+++ src/directives/index.js
@@ -0,0 +1,25 @@
+import { useIntersectionObserver } from '@vueuse/core'
+//懒加载插件
+export const lazyPlugin = {
+  install (app) {
+    app.directive('img-lazy', {
+      mounted (el, binding) {
+        //el：绑定的元素
+        //binding:binding.value:指令等于号后面绑定的表达式的值 v-img-lazy = "" 引号里面的内容
+        // console.log(el)
+        // console.log(binding)
+        useIntersectionObserver(
+          el,
+          ([{ isIntersecting }]) => {
+            // console.log(isIntersecting)
+            if (isIntersecting) {
+              //进入
+              el.src = binding.value
+            }
+          }
+        )
+
+      }
+    })
+  }
+}
--- src/main.js
+++ src/main.js
@@ -6,33 +6,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import { useIntersectionObserver } from '@vueuse/core'
+import { lazyPlugin } from './directives'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(lazyPlugin)
 app.mount('#app')
 
-//全局指令
 
-app.directive('img-lazy', {
-  mounted (el, binding) {
-    //el：绑定的元素
-    //binding:binding.value:指令等于号后面绑定的表达式的值 v-img-lazy = "" 引号里面的内容
-    // console.log(el)
-    // console.log(binding)
-    useIntersectionObserver(
-      el,
-      ([{ isIntersecting }]) => {
-        // console.log(isIntersecting)
-        if (isIntersecting) {
-          //进入
-          el.src = binding.value
-        }
-      }
-    )
-
-  }
-})
