6aa6c5d793c9e25ef45b89768bbbd2fe3151eef1
lry6789 <cleansexd1@gmail.com>
2024-04-22 11:16:57 +0800

    完成Home分类

---
+++ src/views/Home/components/HomeBanner.vue
@@ -0,0 +1,4 @@
+
+<template>
+  轮播图
+</template>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,10 +1,23 @@
-<template>
-  <div class = "test-scroll">我是home</div>
+<script setup>
+  import HomeBanner  from './components/HomeBanner.vue'
+  import HomeHot from './components/HomeHot.vue'
+  import HomeCategory from './components/HomeCategory.vue'
+  import HomeNew from './components/HomeNew.vue'
+  import HomeProduct from './components/HomeProduct.vue'
 
+</script>
+<template>
+  <div class = "container">
+    <HomeCategory/>
+    <HomeBanner/>
+  </div>
+  <HomeNew/>
+  <HomeHot/>
+  <HomeProduct/>
 </template>
 
 <style>
   .test-scroll{
     height: 1400px;
   }
-</style>
+</style>./components/HomeBanner.vue
