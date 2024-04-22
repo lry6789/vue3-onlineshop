dc69d0188d3f0e2e4786091e921115a71c909347
lry6789 <cleansexd1@gmail.com>
2024-04-22 13:04:13 +0800

    通用pannel for 使用插槽和slot

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,7 @@
   import HomeCategory from './components/HomeCategory.vue'
   import HomeNew from './components/HomeNew.vue'
   import HomeProduct from './components/HomeProduct.vue'
+  import HomePannel from './components/HomePannel.vue'
 
 </script>
 <template>
@@ -14,6 +15,12 @@
   <HomeNew/>
   <HomeHot/>
   <HomeProduct/>
+  <HomePannel title = '新鲜好物' subTitle = '新鲜好物 好多商品'>
+    
+  </HomePannel>
+  <HomePannel title = '人气推荐' subTitle = '人气推荐 好多商品'>
+    
+  </HomePannel>
 </template>
 
 <style>
