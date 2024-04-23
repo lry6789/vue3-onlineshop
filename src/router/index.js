905666176e6614e2ef7d987975fb2734fa28810c
lry6789 <cleansexd1@gmail.com>
2024-04-23 21:48:09 +0800

    详情页静态与路由

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Login from '@/views/Login/index.vue'
 import Category from '@/views/Category/index.vue'
 import Home from '@/views/Home/index.vue'
 import SubCategory from '@/views/SubCategory/index.vue'
+import Detail from '@/views/Detail/index.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -29,6 +30,10 @@ const router = createRouter({
       path: '/login',
       component: Login
     },
+    {
+      path: '/detail/:id',
+      component: Detail
+    }
   ],
   scrollBehavior () {
     return { top: 0 }
