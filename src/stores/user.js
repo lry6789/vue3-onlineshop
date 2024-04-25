4a396f164d9e6f4b00b0b18129339d9409202cec
lry6789 <cleansexd1@gmail.com>
2024-04-25 11:04:25 +0800

    退出登录

--- src/stores/user.js
+++ src/stores/user.js
@@ -8,14 +8,21 @@ export const useUserStore = defineStore(
     //用户state
     const userInfo = ref({})
     //方法
+    //获取用户信息完成登录
     const getUserInfo = async ({ account, password }) => {
       const res = await loginAPI({ account, password })
       userInfo.value = res.result
     }
+    //退出登录
+    const clearUserInfo = () => {
+      userInfo.value = {}
+    }
+
     //对象格式return
     return {
       userInfo,
-      getUserInfo
+      getUserInfo,
+      clearUserInfo
     }
   },
   {
