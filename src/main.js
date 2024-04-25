ff0d40ecdb180acb286ffab7aa58f04eff5796ed
lry6789 <cleansexd1@gmail.com>
2024-04-25 10:45:18 +0800

    数据持久化

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import '@/styles/common.scss'
 
 import { createApp } from 'vue'
+
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
@@ -10,7 +12,10 @@ import { lazyPlugin } from './directives'
 import { componentsPlugin } from './components'
 const app = createApp(App)
 
-app.use(createPinia())
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
+
 app.use(router)
 app.use(lazyPlugin)
 app.use(componentsPlugin)
